40d1906a6ad12019dce3852f28da25e60de70f81
Ilan ARFI <i.arfipro@outlook.fr>
2026-04-08 18:49:24 +0200

    header swagger

--- app/Rest/Controllers/Controller.php
+++ app/Rest/Controllers/Controller.php
@@ -6,17 +6,17 @@ use Lomkit\Rest\Http\Controllers\Controller as RestController;
 use OpenApi\Attributes as OA;
 
 #[OA\Info(
-    version: '1.0.0',
+    version: '2.0.0',
     description: "Documentation de l'API HealthAI Coach, gérée avec Lomkit REST API.",
     title: 'HealthAI Coach API'
 )]
 #[OA\Server(
-    url: 'http://localhost:80/api',
-    description: 'Serveur de développement local'
+    url: 'https://api-production-1262.up.railway.app:8080/api',
+    description: 'Serveur de développement production'
 )]
 #[OA\SecurityScheme(
     securityScheme: 'sanctum',
-    type: 'http',
+    type: 'https',
     description: 'Entrez votre token Sanctum ici',
     scheme: 'bearer'
 )]
